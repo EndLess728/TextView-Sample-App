f3e3924af79ed23c5f51179a3696d66359df1dad
EndLess728 <aryanmk.mk5@gmail.com>
2018-09-07 11:12:33 +0530

    simple Textview App

--- TexView App/ViewController.swift
+++ TexView App/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mytextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        mytextView.textColor = UIColor.greenColor()
+        mytextView.editable = true
+        mytextView.backgroundColor = UIColor.grayColor()
+        mytextView.font = UIFont.boldSystemFontOfSize(20)
+    
+        //print("This is test")
+    
     }
+    
+
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+ 
+        
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
 }
 
